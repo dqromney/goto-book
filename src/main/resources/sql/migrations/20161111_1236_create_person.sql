058fee4ce67d4abbe4d46f26ecae152c38585bc5
Heather Romney <heatherromney@gmail.com>
2016-11-11 00:40:41 -0700

    Added Flyway integration.

---
+++ src/main/resources/sql/migrations/20161111_1236_create_person.sql
@@ -0,0 +1,6 @@
+CREATE  TABLE `tgtb`.`person` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `personname` VARCHAR(45) NULL ,
+  `personage` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
